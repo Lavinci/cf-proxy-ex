1d62af4b1468cdafba5760ada81e92552fe46ab2
Lavinci <lavinci@yeah.net>
2024-06-22 16:01:45 +0000

    add default

--- _worker.js
+++ _worker.js
@@ -287,208 +287,211 @@ const redirectError = `
 `;
 
 //new URL(请求路径, base路径).href;
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  //var siteOnly = url.pathname.substring(url.pathname.indexOf(str) + str.length);
-
-  var actualUrlStr = url.pathname.substring(url.pathname.indexOf(str) + str.length) + url.search + url.hash;
-  if (actualUrlStr == "") { //先返回引导界面
-    return getHTMLResponse(mainPage);
-  }
-
-
-  try {
-    var test = actualUrlStr;
-    if (!test.startsWith("http")) {
-      test = "https://" + test;
+export default{
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    //var siteOnly = url.pathname.substring(url.pathname.indexOf(str) + str.length);
+  
+    var actualUrlStr = url.pathname.substring(url.pathname.indexOf(str) + str.length) + url.search + url.hash;
+    if (actualUrlStr == "") { //先返回引导界面
+      return getHTMLResponse(mainPage);
     }
-    var u = new URL(test);
-    if (!u.host.includes(".")) {
-      throw new Error();
-    }
-  }
-  catch { //可能是搜素引擎，比如proxy.com/https://www.duckduckgo.com/ 转到 proxy.com/?q=key
-    var siteCookie = request.headers.get('Cookie');
-    var lastVisit;
-    if (siteCookie != null && siteCookie != "") {
-      lastVisit = getCook(proxyCookie, siteCookie);
-      console.log(lastVisit);
-      if (lastVisit != null && lastVisit != "") {
-        //(!lastVisit.startsWith("http"))?"https://":"" + 
-        //现在的actualUrlStr如果本来不带https:// 的话那么现在也不带，因为判断是否带protocol在后面
-        return Response.redirect(thisProxyServerUrlHttps + lastVisit + "/" + actualUrlStr, 301);
-      }
-    }
-    return getHTMLResponse("Something is wrong while trying to get your cookie: <br> siteCookie: " + siteCookie + "<br>" + "lastSite: " + lastVisit);
-  }
-
-
-
-  if (!actualUrlStr.startsWith("http") && !actualUrlStr.includes("://")) { //从www.xxx.com转到https://www.xxx.com
-    //actualUrlStr = "https://" + actualUrlStr;
-    return Response.redirect(thisProxyServerUrlHttps + "https://" + actualUrlStr, 301);
-  }
-  //if(!actualUrlStr.endsWith("/")) actualUrlStr += "/";
-  const actualUrl = new URL(actualUrlStr);
-
-  let clientHeaderWithChange = new Headers();
-  //***代理发送数据的Header：修改部分header防止403 forbidden，要先修改，   因为添加Request之后header是只读的（***ChatGPT，未测试）
-  for (var pair of request.headers.entries()) {
-    //console.log(pair[0]+ ': '+ pair[1]);
-    clientHeaderWithChange.set(pair[0], pair[1].replaceAll(thisProxyServerUrlHttps, actualUrlStr).replaceAll(thisProxyServerUrl_hostOnly, actualUrl.host));
-  }
-
-  let clientRequestBodyWithChange
-  if (request.body) {
-    clientRequestBodyWithChange = await request.text();
-    clientRequestBodyWithChange = clientRequestBodyWithChange
-      .replaceAll(thisProxyServerUrlHttps, actualUrlStr)
-      .replaceAll(thisProxyServerUrl_hostOnly, actualUrl.host);
-  }
-
-  const modifiedRequest = new Request(actualUrl, {
-    headers: clientHeaderWithChange,
-    method: request.method,
-    body: (request.body) ? clientRequestBodyWithChange : request.body,
-    //redirect: 'follow'
-    redirect: "manual"
-    //因为有时候会
-    //https://www.jyshare.com/front-end/61   重定向到
-    //https://www.jyshare.com/front-end/61/
-    //但是相对目录就变了
-  });
-
-  //console.log(actualUrl);
-
-  const response = await fetch(modifiedRequest);
-  if (response.status.toString().startsWith("3") && response.headers.get("Location") != null) {
-    //console.log(base_url + response.headers.get("Location"))
+  
+  
     try {
-      return Response.redirect(thisProxyServerUrlHttps + new URL(response.headers.get("Location"), actualUrlStr).href, 301);
-    } catch {
-      getHTMLResponse(redirectError + "<br>the redirect url:" + response.headers.get("Location") + ";the url you are now at:" + actualUrlStr);
-    }
-  }
-
-  var modifiedResponse;
-  var bd;
-
-  const contentType = response.headers.get("Content-Type");
-  if (contentType && contentType.startsWith("text/")) {
-    bd = await response.text();
-
-    //ChatGPT
-    let regex = new RegExp(`(?<!src="|href=")(https?:\\/\\/[^\s'"]+)`, 'g');
-    bd = bd.replace(regex, (match) => {
-      if (match.includes("http")) {
-        return thisProxyServerUrlHttps + match;
-      } else {
-        return thisProxyServerUrl_hostOnly + "/" + match;
+      var test = actualUrlStr;
+      if (!test.startsWith("http")) {
+        test = "https://" + test;
       }
-    });
-
-    console.log(bd); // 输出替换后的文本
-
-    //bd.includes("<html")  //不加>因为html标签上可能加属性         这个方法不好用因为一些JS中竟然也会出现这个字符串
-    //也需要加上这个方法因为有时候server返回json也是html
-    if (contentType && contentType.includes("text/html") && bd.includes("<html")) {
-      //console.log("STR" + actualUrlStr)
-      bd = covToAbs(bd, actualUrlStr);
-      bd = removeIntegrityAttributes(bd);
-      bd = "<script>" + httpRequestInjection + "</script>" + bd;
-    }
-    //else{
-    //   //const type = response.headers.get('Content-Type');type == null || (type.indexOf("image/") == -1 && type.indexOf("application/") == -1)
-    //   if(actualUrlStr.includes(".css")){ //js不用，因为我已经把网络消息给注入了
-    //     for(var r of CSSReplace){
-    //       bd = bd.replace(r, thisProxyServerUrlHttps + r);
-    //     }
-    //   }
-    //   //问题:在设置css background image 的时候可以使用相对目录  
-    // }
-    //console.log(bd);
-
-    modifiedResponse = new Response(bd, response);
-  } else {
-    var blob = await response.blob();
-    modifiedResponse = new Response(blob, response);
-  }
-
-
-
-
-  let headers = modifiedResponse.headers;
-  let cookieHeaders = [];
-
-  // Collect all 'Set-Cookie' headers regardless of case
-  for (let [key, value] of headers.entries()) {
-    if (key.toLowerCase() == 'set-cookie') {
-      cookieHeaders.push({ headerName: key, headerValue: value });
-    }
-  }
-
-
-  if (cookieHeaders.length > 0) {
-    cookieHeaders.forEach(cookieHeader => {
-      let cookies = cookieHeader.headerValue.split(',').map(cookie => cookie.trim());
-
-      for (let i = 0; i < cookies.length; i++) {
-        let parts = cookies[i].split(';').map(part => part.trim());
-        //console.log(parts);
-
-        // Modify Path
-        let pathIndex = parts.findIndex(part => part.toLowerCase().startsWith('path='));
-        let originalPath;
-        if (pathIndex !== -1) {
-          originalPath = parts[pathIndex].substring("path=".length);
-        }
-        let absolutePath = "/" + new URL(originalPath, actualUrlStr).href;;
-
-        if (pathIndex !== -1) {
-          parts[pathIndex] = `Path=${absolutePath}`;
-        } else {
-          parts.push(`Path=${absolutePath}`);
-        }
-
-        // Modify Domain
-        let domainIndex = parts.findIndex(part => part.toLowerCase().startsWith('domain='));
-
-        if (domainIndex !== -1) {
-          parts[domainIndex] = `domain=${thisProxyServerUrl_hostOnly}`;
-        } else {
-          parts.push(`domain=${thisProxyServerUrl_hostOnly}`);
-        }
-
-        cookies[i] = parts.join('; ');
+      var u = new URL(test);
+      if (!u.host.includes(".")) {
+        throw new Error();
       }
-
-      // Re-join cookies and set the header
-      headers.set(cookieHeader.headerName, cookies.join(', '));
+    }
+    catch { //可能是搜素引擎，比如proxy.com/https://www.duckduckgo.com/ 转到 proxy.com/?q=key
+      var siteCookie = request.headers.get('Cookie');
+      var lastVisit;
+      if (siteCookie != null && siteCookie != "") {
+        lastVisit = getCook(proxyCookie, siteCookie);
+        console.log(lastVisit);
+        if (lastVisit != null && lastVisit != "") {
+          //(!lastVisit.startsWith("http"))?"https://":"" + 
+          //现在的actualUrlStr如果本来不带https:// 的话那么现在也不带，因为判断是否带protocol在后面
+          return Response.redirect(thisProxyServerUrlHttps + lastVisit + "/" + actualUrlStr, 301);
+        }
+      }
+      return getHTMLResponse("Something is wrong while trying to get your cookie: <br> siteCookie: " + siteCookie + "<br>" + "lastSite: " + lastVisit);
+    }
+  
+  
+  
+    if (!actualUrlStr.startsWith("http") && !actualUrlStr.includes("://")) { //从www.xxx.com转到https://www.xxx.com
+      //actualUrlStr = "https://" + actualUrlStr;
+      return Response.redirect(thisProxyServerUrlHttps + "https://" + actualUrlStr, 301);
+    }
+    //if(!actualUrlStr.endsWith("/")) actualUrlStr += "/";
+    const actualUrl = new URL(actualUrlStr);
+  
+    let clientHeaderWithChange = new Headers();
+    //***代理发送数据的Header：修改部分header防止403 forbidden，要先修改，   因为添加Request之后header是只读的（***ChatGPT，未测试）
+    for (var pair of request.headers.entries()) {
+      //console.log(pair[0]+ ': '+ pair[1]);
+      clientHeaderWithChange.set(pair[0], pair[1].replaceAll(thisProxyServerUrlHttps, actualUrlStr).replaceAll(thisProxyServerUrl_hostOnly, actualUrl.host));
+    }
+  
+    let clientRequestBodyWithChange
+    if (request.body) {
+      clientRequestBodyWithChange = await request.text();
+      clientRequestBodyWithChange = clientRequestBodyWithChange
+        .replaceAll(thisProxyServerUrlHttps, actualUrlStr)
+        .replaceAll(thisProxyServerUrl_hostOnly, actualUrl.host);
+    }
+  
+    const modifiedRequest = new Request(actualUrl, {
+      headers: clientHeaderWithChange,
+      method: request.method,
+      body: (request.body) ? clientRequestBodyWithChange : request.body,
+      //redirect: 'follow'
+      redirect: "manual"
+      //因为有时候会
+      //https://www.jyshare.com/front-end/61   重定向到
+      //https://www.jyshare.com/front-end/61/
+      //但是相对目录就变了
     });
+  
+    //console.log(actualUrl);
+  
+    const response = await fetch(modifiedRequest);
+    if (response.status.toString().startsWith("3") && response.headers.get("Location") != null) {
+      //console.log(base_url + response.headers.get("Location"))
+      try {
+        return Response.redirect(thisProxyServerUrlHttps + new URL(response.headers.get("Location"), actualUrlStr).href, 301);
+      } catch {
+        getHTMLResponse(redirectError + "<br>the redirect url:" + response.headers.get("Location") + ";the url you are now at:" + actualUrlStr);
+      }
+    }
+  
+    var modifiedResponse;
+    var bd;
+  
+    const contentType = response.headers.get("Content-Type");
+    if (contentType && contentType.startsWith("text/")) {
+      bd = await response.text();
+  
+      //ChatGPT
+      let regex = new RegExp(`(?<!src="|href=")(https?:\\/\\/[^\s'"]+)`, 'g');
+      bd = bd.replace(regex, (match) => {
+        if (match.includes("http")) {
+          return thisProxyServerUrlHttps + match;
+        } else {
+          return thisProxyServerUrl_hostOnly + "/" + match;
+        }
+      });
+  
+      console.log(bd); // 输出替换后的文本
+  
+      //bd.includes("<html")  //不加>因为html标签上可能加属性         这个方法不好用因为一些JS中竟然也会出现这个字符串
+      //也需要加上这个方法因为有时候server返回json也是html
+      if (contentType && contentType.includes("text/html") && bd.includes("<html")) {
+        //console.log("STR" + actualUrlStr)
+        bd = covToAbs(bd, actualUrlStr);
+        bd = removeIntegrityAttributes(bd);
+        bd = "<script>" + httpRequestInjection + "</script>" + bd;
+      }
+      //else{
+      //   //const type = response.headers.get('Content-Type');type == null || (type.indexOf("image/") == -1 && type.indexOf("application/") == -1)
+      //   if(actualUrlStr.includes(".css")){ //js不用，因为我已经把网络消息给注入了
+      //     for(var r of CSSReplace){
+      //       bd = bd.replace(r, thisProxyServerUrlHttps + r);
+      //     }
+      //   }
+      //   //问题:在设置css background image 的时候可以使用相对目录  
+      // }
+      //console.log(bd);
+  
+      modifiedResponse = new Response(bd, response);
+    } else {
+      var blob = await response.blob();
+      modifiedResponse = new Response(blob, response);
+    }
+  
+  
+  
+  
+    let headers = modifiedResponse.headers;
+    let cookieHeaders = [];
+  
+    // Collect all 'Set-Cookie' headers regardless of case
+    for (let [key, value] of headers.entries()) {
+      if (key.toLowerCase() == 'set-cookie') {
+        cookieHeaders.push({ headerName: key, headerValue: value });
+      }
+    }
+  
+  
+    if (cookieHeaders.length > 0) {
+      cookieHeaders.forEach(cookieHeader => {
+        let cookies = cookieHeader.headerValue.split(',').map(cookie => cookie.trim());
+  
+        for (let i = 0; i < cookies.length; i++) {
+          let parts = cookies[i].split(';').map(part => part.trim());
+          //console.log(parts);
+  
+          // Modify Path
+          let pathIndex = parts.findIndex(part => part.toLowerCase().startsWith('path='));
+          let originalPath;
+          if (pathIndex !== -1) {
+            originalPath = parts[pathIndex].substring("path=".length);
+          }
+          let absolutePath = "/" + new URL(originalPath, actualUrlStr).href;;
+  
+          if (pathIndex !== -1) {
+            parts[pathIndex] = `Path=${absolutePath}`;
+          } else {
+            parts.push(`Path=${absolutePath}`);
+          }
+  
+          // Modify Domain
+          let domainIndex = parts.findIndex(part => part.toLowerCase().startsWith('domain='));
+  
+          if (domainIndex !== -1) {
+            parts[domainIndex] = `domain=${thisProxyServerUrl_hostOnly}`;
+          } else {
+            parts.push(`domain=${thisProxyServerUrl_hostOnly}`);
+          }
+  
+          cookies[i] = parts.join('; ');
+        }
+  
+        // Re-join cookies and set the header
+        headers.set(cookieHeader.headerName, cookies.join(', '));
+      });
+    }
+    //bd != null && bd.includes("<html")
+    if (contentType && contentType.includes("text/html") && response.status == 200 && bd.includes("<html")) { //如果是HTML再加cookie，因为有些网址会通过不同的链接添加CSS等文件
+      let cookieValue = proxyCookie + "=" + actualUrl.origin + "; Path=/; Domain=" + thisProxyServerUrl_hostOnly;
+      //origin末尾不带/
+      //例如：console.log(new URL("https://www.baidu.com/w/s?q=2#e"));
+      //origin: "https://www.baidu.com"
+      headers.append("Set-Cookie", cookieValue);
+    }
+  
+    // 添加允许跨域访问的响应头
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+    //modifiedResponse.headers.set("Content-Security-Policy", "default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data:; media-src *; frame-src *; font-src *; connect-src *; base-uri *; form-action *;");
+    if (modifiedResponse.headers.has("Content-Security-Policy")) {
+      modifiedResponse.headers.delete("Content-Security-Policy");
+    }
+    if (modifiedResponse.headers.has("Permissions-Policy")) {
+      modifiedResponse.headers.delete("Permissions-Policy");
+    }
+    modifiedResponse.headers.set("X-Frame-Options", "ALLOWALL");
+  
+    return modifiedResponse;
   }
-  //bd != null && bd.includes("<html")
-  if (contentType && contentType.includes("text/html") && response.status == 200 && bd.includes("<html")) { //如果是HTML再加cookie，因为有些网址会通过不同的链接添加CSS等文件
-    let cookieValue = proxyCookie + "=" + actualUrl.origin + "; Path=/; Domain=" + thisProxyServerUrl_hostOnly;
-    //origin末尾不带/
-    //例如：console.log(new URL("https://www.baidu.com/w/s?q=2#e"));
-    //origin: "https://www.baidu.com"
-    headers.append("Set-Cookie", cookieValue);
-  }
-
-  // 添加允许跨域访问的响应头
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-  //modifiedResponse.headers.set("Content-Security-Policy", "default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data:; media-src *; frame-src *; font-src *; connect-src *; base-uri *; form-action *;");
-  if (modifiedResponse.headers.has("Content-Security-Policy")) {
-    modifiedResponse.headers.delete("Content-Security-Policy");
-  }
-  if (modifiedResponse.headers.has("Permissions-Policy")) {
-    modifiedResponse.headers.delete("Permissions-Policy");
-  }
-  modifiedResponse.headers.set("X-Frame-Options", "ALLOWALL");
-
-  return modifiedResponse;
+  
 }
+
 function escapeRegExp(string) {
   return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& 表示匹配的字符
 }
